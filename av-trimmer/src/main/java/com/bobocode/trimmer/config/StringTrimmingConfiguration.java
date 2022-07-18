4dfd27cba975c29cd7e4781f6896607b87e671f7
Oleksandr Vashchenko <oleksandr.vashchenko@sap.com>
2022-07-18 16:21:27 -0400

    av-trimmer

---
+++ av-trimmer/src/main/java/com/bobocode/trimmer/config/StringTrimmingConfiguration.java
@@ -0,0 +1,13 @@
+package com.bobocode.trimmer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StringTrimmingConfiguration {
+
+    @Bean
+    public TrimmedAnnotationBeanPostProcessor trimPostProcessor() {
+        return new TrimmedAnnotationBeanPostProcessor();
+    }
+}
---
+++ av-trimmer/src/main/java/com/bobocode/trimmer/config/TrimmedAnnotationBeanPostProcessor.java
@@ -0,0 +1,34 @@
+package com.bobocode.trimmer.config;
+
+import com.bobocode.trimmer.annotation.Trimmed;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+
+import java.util.Arrays;
+
+public class TrimmedAnnotationBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        final var beanType = bean.getClass();
+
+        if (beanType.isAnnotationPresent(Trimmed.class)) {
+            final var enhancer = new Enhancer();
+            enhancer.setSuperclass(beanType);
+
+            final MethodInterceptor interceptor = (obj, method, args, methodProxy) -> {
+                args = Arrays.stream(args).map(s -> s instanceof String ? ((String) s).trim() : s).toArray();
+
+                var returnValue = methodProxy.invokeSuper(obj, args);
+                return returnValue instanceof String ? ((String) returnValue).trim() : returnValue;
+            };
+
+            enhancer.setCallback(interceptor);
+            return enhancer.create();
+        }
+
+        return bean;
+    }
+}
