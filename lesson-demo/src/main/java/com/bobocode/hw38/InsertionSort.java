4fc5170c5af932dfc551d6ba00e2de574c94eb83
Oleksandr Vashchenko <oleksandr.vashchenko@sap.com>
2022-10-06 00:31:38 -0500

    update hw

--- lesson-demo/src/main/java/com/bobocode/hw38/InsertionSort.java
+++ lesson-demo/src/main/java/com/bobocode/hw38/InsertionSort.java
@@ -1,2 +1,34 @@
-package com.bobocode.hw38;public class InsertionSort {
+package com.bobocode.hw38;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        var integers = List.of(1, 4, 3, 1, 9, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4);
+        var result = sort(integers, Comparator.comparingInt(Integer::intValue));
+
+        System.out.println(result);
+    }
+
+    public static <T> List<T> sort(List<T> list, Comparator<T> cmp) {
+        list = new ArrayList<>(list);
+
+        for (var i = 1; i < list.size(); i++) {
+            var picked = list.get(i);
+            var currentPosition = i;
+
+            while (currentPosition > 0
+                    && cmp.compare(picked, list.get(currentPosition - 1)) < 0) {
+                list.set(currentPosition, list.get(currentPosition - 1));
+                currentPosition--;
+            }
+
+            list.set(currentPosition, picked);
+        }
+
+        return list;
+    }
 }
